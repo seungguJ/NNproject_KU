e9731c9688c36f71b9cf8dbce07fc8616b7344ba
seung_gu <127372942+seungguJ@users.noreply.github.com>
2023-09-25 13:34:16 +0900

    Update Training_ResNet18.py

--- NNproject/Training_ResNet18.py
+++ NNproject/Training_ResNet18.py
@@ -1,56 +1,69 @@
 import torch
 import torchvision.transforms as transforms
 import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.models as models
 
-def attack(imgs, l2 = True, eps = 8/255, device=False):
-    if l2:
-        noise = torch.normal(mean = 0., std = 0.1, size=(imgs.shape[0],3*32*32)).to(device)
-        condi = abs(eps)**0.5
-        noise = torch.where(noise > condi, condi, noise)
-        noise = torch.where(noise < -condi, -condi, noise)
-        noise = noise.view(imgs.shape[0], 3, 32, 32)
-        adv_imgs = imgs + noise
-        adv_imgs = torch.clamp(adv_imgs, 0, 1)
-    else:
-        noise = torch.normal(mean = 0., std = 0.1, size=(imgs.shape[0],3*32*32)).to(device)
-        noise = torch.clamp(noise, -eps, eps)
-        noise = noise.view(imgs.shape[0], 3, 32, 32)
-        adv_imgs = imgs + noise
-        adv_imgs = torch.clamp(adv_imgs, 0, 1)
-    return adv_imgs
-    
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f'{device} is available')
 
 transform = transforms.Compose([
+    transforms.RandomCrop(32,padding=4),
     transforms.ToTensor(),
 ])
 
-batch_size = 100
+trainset = torchvision.datasets.CIFAR10(root='...data_path...', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
-testset = torchvision.datasets.CIFAR10(root='...data_path...', train=False, download=False, transform=transform)
-testloader = torch.utils.data.DataLoader(testset,batch_size=batch_size, shuffle=False)
+testset = torchvision.datasets.CIFAR10(root='...data_path...', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset,batch_size=64, shuffle=False)
 
-model = torch.load('...model_path.../resnet18_cifar10.pt')
-model = model.cuda()
-model.eval()
+model = models.resnet18(pretrained=False)
+model.to(device)
 
-correct, adv_correct = 0, 0
-total, adv_total = 0, 0
-with torch.no_grad(): # Deactivate gradient calculation
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 10).to(device)
+
+critertion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(),lr=0.001,momentum=0.9)
+
+num_epochs = 200
+loss_ = []
+n = len(trainloader)
+
+model.train()
+
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    
+    for i, (images, labels) in enumerate(trainloader):
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        
+        outputs = model(images)
+        
+        loss = critertion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    
+    loss_.append(running_loss/n)
+    print(f'{epoch+1} loss: {running_loss/n}')
+    
+# prediction part
+correct = 0
+total = 0
+with torch.no_grad(): 
   model.eval() 
   for data in testloader:
     images, labels = data[0].to(device), data[1].to(device)
-    adv_images = attack(images, device=device)
-    # For infinity norm attack
-    # adv_imges = attack(imges,l2=False, eps=0.05 device=device)
     outputs = model(images)
-    adv_outputs = model(adv_images)
-    _, predicted = torch.max(outputs.data, 1) # Probability, index
-    _, adv_predicted = torch.max(adv_outputs.data, 1) # Probability, index
-    total += labels.size(0) # Number of labels
-    adv_total += labels.size(0) # Number of labels
-    correct += (predicted == labels).sum().item() # Number of correct predictions
-    adv_correct += (adv_predicted == labels).sum().item() # Number of correct predictions
+    _, predicted = torch.max(outputs.data, 1) 
+    total += labels.size(0) 
+    correct += (predicted == labels).sum().item()
 
-print(correct/total, adv_correct/total) # Accuracy
+print(f'accuracy of 10000 test images: {100*correct/total}%')
+
+PATH = '...checkpoint_path...'
+torch.save(model, PATH + 'resnet18_cifar10.pt')
